14645395d5734423feb322e627ccb19bc1f3d9cc
GuggilamKrishnaSree <143433420+GuggilamKrishnaSree@users.noreply.github.com>
2024-06-23 14:53:08 +0530

    Injected the forecast from JavaScript

--- src/app.js
+++ src/app.js
@@ -45,7 +45,26 @@ function handleSearchSubmit(event)
     searchCity(searchInput.value);
 }
 
+function displayForecast(){
+    let days = ["Tue","Wed","Thu","Fri","Sat"];
+    let forecastHTML = "";
+    
+    days.forEach(function (day){
+        forecastHTML = forecastHTML + `<div class="weather-forecast-day">
+                    <div class="weather-forecast-date">${day}</div>
+                    <div class="weather-forecast-icon"></div>
+                    <div class="weather-forecast-temperatures">
+                        <span class="weather-forecast-temperature-max">18°</span>
+                        <span class="weather-forecast-temperature-min">12</span>
+                    </div>
+                </div>`;
+    });
+    let forecastElement = document.querySelector("#forecast");
+    forecastElement.innerHTML = forecastHTML;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit",handleSearchSubmit);
 
 searchCity("Delhi");
+displayForecast();
